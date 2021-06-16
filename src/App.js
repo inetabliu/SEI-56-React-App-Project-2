0bd14eab4dc340180f430c9586a64522244b6335
Your Name <ineta.bliu@gmail.com>
2021-06-16 19:40:01 +0100

    First page of characters displaying
    Co-authored-by: VaniaTominc <VaniaTominc@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react'
+
+import axios from 'axios'
+
+
 function App() {
-  return <h1>Hello World</h1>
+  
+  const [character, setCharacter] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get('https://rickandmortyapi.com/api/character/')
+      // console.log('data ', data.results)
+      setCharacter(data.results)
+      // console.log('character', character)
+    }
+    getData()
+  }, [character])
+  
+
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      {character.map((item, index) => {
+        return <div key={index}>
+          <p>{item.name}</p>
+          <p>{item.status}</p>
+          <p>{item.species}</p>
+          <p>{item.gender}</p>
+          <img src={item.image} alt={item.name} />
+        </div>
+      }) 
+      }
+    </>
+  )
+  
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/main.scss'
 import App from './App'
+import 'bulma'
+
 
 ReactDOM.render(
   <App />,

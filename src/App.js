1326276dc8ca01cfcb4975877fc18be686db785a
Your Name <ineta.bliu@gmail.com>
2021-06-16 22:46:08 +0100

    Condition for species & status return right selection Co-authored-by: VaniaTominc <VaniaTominc@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -7,29 +7,68 @@ import axios from 'axios'
 function App() {
   
   const [character, setCharacter] = useState([])
+  const [filteredCharacter, setFilteredCharacter] = useState([])
 
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios.get('https://rickandmortyapi.com/api/character/')
-      // console.log('data ', data.results)
+      const { data } = await axios.get('https://rickandmortyapi.com/api/character')
       setCharacter(data.results)
-      // console.log('character', character)
     }
     getData()
-  }, [character])
-  
+  }, [])
 
+ 
+
+
+  const handleChange = (event) => {
+   
+    const filteredArray = character.filter(item => {
+      return (item.status === event.target.value) || (item.species === event.target.value)
+   
+      
+      
+    })
+    setCharacter(filteredArray)
+  
+    
+  }
+
+  
 
   return (
     <>
-      <h1>Hello World</h1>
+      <section>
+        <div>
+          <div> Pick the livelines status
+            <br />
+            <label htmlFor='alive'>Alive</label>
+            <input onClick={handleChange} type="radio" id='alive' name='status'  value='Alive'/>
+            <br />
+            <label htmlFor='dead'>Dead</label>
+            <input onClick={handleChange} type="radio" id='dead' name='status' value='Dead' />
+            <br />
+            <label htmlFor='unknown'>Unknown</label>
+            <input onClick={handleChange} type="radio" id='unknown' name='status' value='unknown'/>
+          </div>
+          <div>
+            <label htmlFor='human'>Human</label>
+            <input onClick={handleChange} type="radio" id='human' name='species'  value='Human'/>
+            <br />
+            <label htmlFor='alien'>Alien</label>
+            <input onClick={handleChange} type="radio" id='alien' name='species' value='Alien' />
+            <br />
+          </div>
+        </div>
+      </section>
+      
       {character.map((item, index) => {
         return <div key={index}>
-          <p>{item.name}</p>
-          <p>{item.status}</p>
-          <p>{item.species}</p>
-          <p>{item.gender}</p>
+          <p>Name: {item.name}</p>
           <img src={item.image} alt={item.name} />
+          <p>Status: {item.status}</p>
+          <p>Species: {item.species}</p>
+          <p>Gender: {item.gender}</p>
+          <p>Planet: {item.origin.name}</p>
         </div>
       }) 
       }

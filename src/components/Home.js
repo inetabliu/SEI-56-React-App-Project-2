6bdb62920f447131cc68e6a8f675704723845ac7
Ineta Bliudziute <81913593+inetabliu@users.noreply.github.com>
2021-08-12 16:02:35 +0100

    new build before readme

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import useSound from 'use-sound'
 import Sound from '../sounds/allaboardmf.wav'
 
+
 const Home = () => {
 
   //Play sound on entering the characters page
@@ -14,9 +15,8 @@ const Home = () => {
       <section className="hero is-fullheight-with-navbar">
         <div className="hero-body">
           <div className="container">
-            <h1 className="title is-1 has-text-centered">
-              <Link onClick={play}to="/characters"><img className='inital-image' src='https://thumbs.gfycat.com/BareJoyousAsp.webp' alt='initialGif' /></Link>
-            </h1>
+            <Link onClick={play}to="/characters"><img className='inital-image' src='https://thumbs.gfycat.com/BareJoyousAsp.webp' alt='initialGif' /></Link>
+            <h1 className="larger is-responsive">Welcome to Rick & Morty Character World</h1>
           </div>
         </div>
       </section>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-
+  const [navState, setNavState] = useState(false)
 
   //Reloads the random character page
   const handleClickRandom = () => {
